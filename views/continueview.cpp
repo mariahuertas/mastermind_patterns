1f931e073250761ae9e4808fcdcc38eeaaef28f0
maria <mariahuertas296@gmail.com>
2019-06-11 19:27:33 +0200

    Add assert in Views

--- views/continueview.cpp
+++ views/continueview.cpp
@@ -2,8 +2,10 @@
 #include <cstdlib>
 #include <ctype.h>
 #include <iostream>
+#include <assert.h>
 
 void ContinueView::interact(ContinueController *continueController){
+    assert(continueController!= nullptr);
     char option;
     do{
         std::cout << "¿Do you want to start a new round?" << std::endl;
--- views/gameview.cpp
+++ views/gameview.cpp
@@ -1,6 +1,7 @@
 #include "gameview.h"
 
 void GameView:: interact(ColocateController *colocateController){
+    assert(colocateController!= nullptr);
     std::cout<<"\nInsert 4 colors: Red(R), Green(G), Purple(P), Brown(B), Yellow(Y), Orange(O)"<<std::endl;
     ProposedCombinationView *proposedCombinationView = new ProposedCombinationView (colocateController);
 
@@ -10,10 +11,12 @@ void GameView:: interact(ColocateController *colocateController){
 }
 
 void GameView:: readCombination(ProposedCombinationView *proposedCombinationView){
+    assert(proposedCombinationView!= nullptr);
     proposedCombinationView->readCombination();
 }
 
 void GameView:: showGame(ProposedCombinationView *proposedCombinationView, ColocateController *colocateController){
+    assert(proposedCombinationView!= nullptr && colocateController != nullptr);
     proposedCombinationView->printCombination();
     if(colocateController->isWinner()){
         std::cout << "GANADOR"<<std::endl;
